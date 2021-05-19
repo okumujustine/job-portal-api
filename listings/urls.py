513a58c21da498a36ad8f69d1da6ed01cd2c2abe
okumujustine <okumujustine01@gmail.com>
2021-05-19 16:53:18 +0300

    added api documentation

--- listings/urls.py
+++ listings/urls.py
@@ -4,8 +4,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static, re_path
 from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
-from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,12 @@ urlpatterns = [
 
     path('joblisting/', include('job_listing_api.urls', namespace='job_listing_api')),
     path("contactus/", include('contactus.urls')),
+    path("", include_docs_urls(title="JobsUg API")),
+    path('schema', get_schema_view(
+        title="JobsUg API",
+        description="API for JobsUg Application",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
 
 if settings.DEBUG:
