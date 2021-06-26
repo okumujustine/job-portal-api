552500061bf47fc34f5bbe617bb942e4fd770f8e
okumujustine <okumujustine01@gmail.com>
2021-06-26 20:25:42 +0300

    adjusted email function

--- authentication/views.py
+++ authentication/views.py
@@ -47,7 +47,7 @@ class RegisterView(generics.GenericAPIView):
 
         user = CustomUser.objects.get(email=user_data['email'])
 
-        Util.send_activation_link(user, request)
+        Util.send_activation_link(user)
         # // send activation link disabled
 
         return Response(user_data, status=status.HTTP_201_CREATED)
